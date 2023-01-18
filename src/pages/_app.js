f772e64403a2aa70a7d45292db57a053fad0f6c5
Geras4323 <german432yo@gmail.com>
2023-01-18 17:45:59 -0300

    [ADD] Added Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,16 @@ import '../styles/globals.css';
 import { AppContext } from '../contexts/AppContext';
 import { useInitialState } from '../hooks/useInitialState';
 
+import { Analytics } from '@vercel/analytics/react';
+
+
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
 
   return (
     <AppContext.Provider value={initialState}>
       <Component {...pageProps} />
+      <Analytics />
     </AppContext.Provider>
   )
 }
